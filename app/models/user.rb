f229b22b17a45462c7b36fbb693999c6edd6f2ec
daison324 <daison.ueki.324@gmail.com>
2021-05-27 11:26:39 +0900

    モデルの単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,25 @@
 class User < ApplicationRecord
-devise :database_authenticatable, :registerable,
-       :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-with_options presence: true do
-    validates :nickname
-    validates :birthday
-  end
+    with_options presence: true do
+      validates :nickname
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'Full-width characters' } do
-    validates :last_name
-    validates :first_name
-  end
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Full-width katakana characters' } do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
+      with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Full-width characters' } do
+        validates :first_name
+        validates :last_name
+      end
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+      with_options format: { with: /\A[ァ-ヶー]+\z/, message: 'Full-width characters' } do
+        validates :first_name_kana
+        validates :last_name_kana
+      end
+    end
+    validates :birthday, presence: true
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 
-  has_many :items
-  has_many :comments
-
+    has_many :items
 end
