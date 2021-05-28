55290e57fea0cbb244ea8662bc554ef4126e2bea
daison324 <daison.ueki.324@gmail.com>
2021-05-28 18:47:59 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,35 +1,28 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
 
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
-  
-    def index
-    end
-  
-  end
-    def new
-      @item = Item.new
-    end
-  
-    def create
-      if @item.valid?
-        @item.save
-        redirect_to root_path
-        redirect_to root_path
-      else
-        render :new
-      
-    end
-  
-  
-    private
-  
-    def item_params
-      params.require(:item).permit(:image, :title, :text, :category_id, :state_id, :fee_id, :prefecture_id, :days_ship_id, :price).merge(user_id: current_user.id)
+  def index
   end
 
-   def contributor_confirmation
-    redirect_to root_path unless current_user == @item.user
+  def new
+    @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render new_item_path
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :text, :prefectures_id, :category_id, :fee_id, :state_id, :days_ship_id, :price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,32 +1,36 @@
 class Item < ApplicationRecord
 
-  validates :image, presence: true
-
-  with_options presence: true do
-    validates :title, length: { maximum: 40 }
-    validates :text, length: { maximum: 1000 }
-    validates :price, numericality: { only_integer: true, message: 'Half-width number'}
-    validates_inclusion_of :price, in: 300..9999999, message: 'Out of setting range'
-  end
-
-  with_options numericality: { other_than: 0 , message: 'Select'} do
-    validates :category_id
-    validates :state_id
-    validates :fee_id
-    validates :prefecture_id
-    validates :days_ship_id
-  end
-
-  has_one_attached :image
-  belongs_to :user
-  has_one :order
-  has_many :comments
-  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :state
   belongs_to :fee
   belongs_to :prefecture
   belongs_to :days_ship
+
+  has_one_attached :image
+  belongs_to :user
+  #has_one :order
+  #has_many :comments
+
+  #validates :image, presence: true
+
+   #with_options presence: true do
+    #validates :title, length: { maximum: 40 }
+    #validates :text, length: { maximum: 1000 }
+    #validates :price, numericality: { only_integer: true, message: 'Half-width number'}
+    #validates_inclusion_of :price, in: 300..9999999, message: 'Out of setting range'
+  #end
+
+  #with_options numericality: { other_than: 0 , message: 'Select'} do
+    #validates :category_id
+    #validates :state_id
+    #validates :fee_id
+    #validates :prefectures_id
+    #validates :days_ship_id
+  #end
+
+
+  
+
   
 end
