10e67b563aa10725ae8b0a9fe1349682bfbc4a1f
daison324 <daison.ueki.324@gmail.com>
2021-05-31 17:43:48 +0900

    画面遷移修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+    title         {Faker::Name.initials}
+    text          {Faker::Lorem.sentence}
+    category_id   {1}
+    state_id      {2}
+    fee_id        {1}
+    prefectures_id {2}
+    days_ship_id  {1}
+    price         {345}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('spec/images/test.jpg'), filename: 'test.jpg')
+    end
   end
+
 end
