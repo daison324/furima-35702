00f09a8a40c3586e5b815f931809a16816a65129
daison324 <daison.ueki.324@gmail.com>
2021-06-02 11:23:42 +0900

    レビュー修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,9 +50,5 @@ class ItemsController < ApplicationController
     redirect_to root_path unless @item.user == current_user
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
+
 end
